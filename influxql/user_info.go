9a1ca25bd76d47e7bf4e8d22735f4b3451ce4ff1
Jonathan A. Sternberg <jonathan@influxdb.com>
2016-06-06 16:55:45 -0500

    Add user information to execution options
    SHOW QUERIES now shows the user information of who ran the query.

---
+++ influxql/user_info.go
@@ -0,0 +1,29 @@
+package influxql
+
+// UserInfo is an interface that holds user information.
+type UserInfo interface {
+	// Name returns the name of the user.
+	Name() string
+
+	// IsAdmin returns if this user is an admin.
+	IsAdmin() bool
+
+	// Authorize checks if the user is authorized to access the database.
+	Authorize(privilege Privilege, database string) bool
+}
+
+// anonymousUser is a user that has all privileges and no name.
+// The anonymousUser is what gets set when no user is set in the execution context.
+type anonymousUser struct{}
+
+func (*anonymousUser) Name() string {
+	return ""
+}
+
+func (*anonymousUser) IsAdmin() bool {
+	return true
+}
+
+func (*anonymousUser) Authorize(privilege Privilege, database string) bool {
+	return true
+}
